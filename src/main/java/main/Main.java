d1d7ff9fe8bee4570a1c8c3f12e45bb7bd4880bf
Milomir Dragović <hello@milomir.rs>
2020-07-20 18:57:58 +0200

    uses classes from packgage media in Main class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,30 @@
 package main;
 
+import media.Radio;
+import media.Television;
+
 public class Main {
 
     public static void main(String[] args) {
         
+        Television lg = new Television();
+        lg.setTurnOn(true);
+        lg.setCurrentProgram(4);
+        lg.setVolume(50);
+        
+        System.out.println("Trenutni program je: " + lg.getCurrentProgram());
+        
+        Radio myRadio = new Radio();
+        
+        myRadio.setAmFrequency(109);
+        myRadio.setBand('F');
+        myRadio.setFmFrequency(703);
+        
+        myRadio.info();
+        
+        Radio newRadio = new Radio(100.3, 700, 'A');
+        newRadio.info();
+       
     }
 
 }
--- src/main/java/media/Radio.java
+++ src/main/java/media/Radio.java
@@ -4,6 +4,16 @@ public class Radio {
     private double fmFrequency;
     private int amFrequency;
     private char band;
+    
+    public Radio() {
+        
+    }
+
+    public Radio(double fmFrequency, int amFrequency, char band) {
+        this.fmFrequency = fmFrequency;
+        this.amFrequency = amFrequency;
+        this.band = band;
+    }
 
     public double getFmFrequency() {
         return fmFrequency;
@@ -27,6 +37,12 @@ public class Radio {
 
     public void setBand(char band) {
         this.band = band;
+    }
+
+    public void info() {
+        System.out.println("FM frekvencija: " + getFmFrequency());
+        System.out.println("AM frekvencija: " + getAmFrequency());
+        System.out.println("Trenutni talas: " + getBand());
     }
     
     
